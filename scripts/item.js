a2207f7c9480d39c686218ad733bd730b24d3db9
ankihg <hagelin.annika@gmail.com>
2016-01-04 20:44:25 -0800

    added item urls.

--- scripts/item.js
+++ scripts/item.js
@@ -1,7 +1,5 @@
 //item class
 
-alert('welcome');
-
 var items = [];
 
 function Item(tmp) {
@@ -9,7 +7,7 @@ function Item(tmp) {
  this.imgSrc = tmp.imgSrc;
  this.date = tmp.date;
  this.about = tmp.about;
-
+ this.url = tmp.url;
 }
 
 Item.prototype.toHTML = function() {
@@ -18,7 +16,7 @@ Item.prototype.toHTML = function() {
 // this.relDate = parseInt((new Date() - new Date(this.date))/60/60/24/1000);
  this.relDate = parseInt((new Date() - new Date(this.date))/60/60/24/1000);
 
- return '<div class="item"> <img src='+this.imgSrc+'> <p><strong>'+this.name+'</strong><br>  '+this.relDate+' days ago <br><br>'+this.about+"</p></div>";
+ return "<a href='"+this.url+"' target='_blank'>"+'<div class="item"> <img src='+this.imgSrc+'> <p><strong>'+this.name+'</strong><br>  '+this.relDate+' days ago <br><br>'+this.about+"</p></div></a>";
 }
 
 rawData.sort(function(a,b) {
