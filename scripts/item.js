478304d16f5c25ee3800eee18d950f88aa04d7cb
ankihg <hagelin.annika@gmail.com>
2016-01-04 16:37:34 -0800

    add items load

--- scripts/item.js
+++ scripts/item.js
@@ -7,11 +7,14 @@ var items = [];
 function Item(tmp) {
  this.name = tmp.name;
  this.about = tmp.about;
+
+
 }
 
 Item.prototype.toHTML = function() {
  alert(this.name.concat(" to html"))
 
+ return '<h3>'+this.name+'</h3><p>'+this.about+"</p>";
 }
 
 rawData.forEach(function(ele) {
