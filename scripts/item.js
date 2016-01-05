cb8e65e8a70cb400ec3017d37e3cf2cec41c36c9
ankihg <hagelin.annika@gmail.com>
2016-01-04 20:32:25 -0800

    dates added.

--- scripts/item.js
+++ scripts/item.js
@@ -7,16 +7,23 @@ var items = [];
 function Item(tmp) {
  this.name = tmp.name;
  this.imgSrc = tmp.imgSrc;
+ this.date = tmp.date;
  this.about = tmp.about;
-
 
 }
 
 Item.prototype.toHTML = function() {
  //alert(this.name.concat(" to html"))
 
- return '<div class="item"> <img src='+this.imgSrc+'> <p><strong>'+this.name+'</strong><br><br>'+this.about+"</p></div>";
+// this.relDate = parseInt((new Date() - new Date(this.date))/60/60/24/1000);
+ this.relDate = parseInt((new Date() - new Date(this.date))/60/60/24/1000);
+
+ return '<div class="item"> <img src='+this.imgSrc+'> <p><strong>'+this.name+'</strong><br>  '+this.relDate+' days ago <br><br>'+this.about+"</p></div>";
 }
+
+rawData.sort(function(a,b) {
+  return (new Date(b.date)) - (new Date(a.date));
+});
 
 rawData.forEach(function(ele) {
   items.push(new Item(ele));
--- scripts/rawItems.js
+++ scripts/rawItems.js
@@ -3,21 +3,25 @@ var rawData = [
   {
     name: 'PORTLAND BRIDGES',
     imgSrc: 'http://ankihg.ucoz.com/pdx_br/portland_bridges.png',
+    date:'2015-12-10',
     about: 'Like an interstate, the Willamette River tears the metropolis. Cross me to connect. Explore bridges and other crossings in the U.S. state of Oregon.'
   },
   {
     name: 'ZANREADS . INFO',
     imgSrc: 'http://ankihg.ucoz.com/zanreads/zanreads2.png',
+    date:'2015-10-31',
     about: 'My friend and client Zan requested a spooky website to display book reviews. <br> <br>&nbsp; &nbsp; &nbsp; &nbsp; Midnight moon illuminates literary alarm <br>&nbsp; &nbsp; &nbsp; &nbsp; Fiction is found in the forest <br> &nbsp; &nbsp; &nbsp; &nbsp; Ghosts grab ominous novels <br>&nbsp; &nbsp; &nbsp; &nbsp; Eerie reads for a cyberchill to the bone'
   },
   {
     name: 'SEATTLE SKYLINE',
     imgSrc: 'http://ankihg.ucoz.com/seattle_skyline/screenshots/seattle_skyline1.png',
+    date:'2015-10-05',
     about: 'An interactive map to explore the skyline of Seattle, this served as my platform to gain proficiency with <a href="https://developers.google.com/maps/documentation/javascript/" target="_blank">Google Maps Javascript API</a>. It is written in JavaScript and uses several Google Maps features including custom markers, events, and animations. Familiarize yourself or assess your intimacy with the edifices.'
   },
   {
     name: 'WILLAMETTECOLLEGIAN . COM',
     imgSrc: 'http://ankihg.ucoz.com/wu_col/img1.png',
+    date:'2015-3-15',
     about: "I served as webmaster for my university's newspaper. This was my first expoersure the web development. Many of my redesigns remain active."
   },
 ];
