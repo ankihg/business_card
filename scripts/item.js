0b54b1cf4011eca3f564fc70c8b83a6874e6e76d
ankihg <hagelin.annika@gmail.com>
2016-01-04 19:19:58 -0800

    added imgs and personal website link

--- scripts/item.js
+++ scripts/item.js
@@ -6,15 +6,16 @@ var items = [];
 
 function Item(tmp) {
  this.name = tmp.name;
+ this.imgSrc = tmp.imgSrc;
  this.about = tmp.about;
 
 
 }
 
 Item.prototype.toHTML = function() {
- alert(this.name.concat(" to html"))
+ //alert(this.name.concat(" to html"))
 
- return '<h3>'+this.name+'</h3><p>'+this.about+"</p>";
+ return '<div class="item"> <img src='+this.imgSrc+'> <p><strong>'+this.name+'</strong><br><br>'+this.about+"</p></div>";
 }
 
 rawData.forEach(function(ele) {
--- scripts/rawItems.js
+++ scripts/rawItems.js
@@ -1,12 +1,14 @@
 //raw items
 var rawData = [
   {
-    name: 'cat flight',
-    about: 'plz respnd'
+    name: 'PORTLAND BRIDGES',
+    imgSrc: 'http://ankihg.ucoz.com/pdx_br/portland_bridges.png',
+    about: 'Like an interstate, the Willamette River tears the metropolis. Cross me to connect. Explore bridges and other crossings in the U.S. state of Oregon.'
   },
   {
-    name: 'zanreads',
-    about: 'no reply'
+    name: 'ZANREADS . INFO',
+    imgSrc: 'http://ankihg.ucoz.com/zanreads/zanreads2.png',
+    about: 'My friend and client Zan requested a spooky website to display book reviews. <br> <br>&nbsp; &nbsp; &nbsp; &nbsp; Midnight moon illuminates literary alarm <br>&nbsp; &nbsp; &nbsp; &nbsp; Fiction is found in the forest <br> &nbsp; &nbsp; &nbsp; &nbsp; Ghosts grab ominous novels <br>&nbsp; &nbsp; &nbsp; &nbsp; Eerie reads for a cyberchill to the bone'
   },
 
 ];
