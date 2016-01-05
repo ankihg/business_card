bfd851b7d04c9aedd014920e53753696e937f607
ankihg <hagelin.annika@gmail.com>
2016-01-05 12:28:52 -0800

    add more button

--- scripts/item.js
+++ scripts/item.js
@@ -8,11 +8,14 @@ function Item(tmp) {
  this.date = tmp.date;
  this.about = tmp.about;
  this.url = tmp.url;
+ //this.tags = tmp.tags;
 }
 
 Item.prototype.toHTML = function() {
  //alert(this.name.concat(" to html"))
  var $newItem = $('.template').clone();
+ $newItem.addClass('item');
+
  $newItem.css('display', 'block');
  $newItem.css('border-style', 'solid');
  $newItem.css('border-color', 'blue');
@@ -25,7 +28,7 @@ Item.prototype.toHTML = function() {
   $newItem.data('date', this.date);
   $newItem.data('about', this.about);
   $newItem.data('url', this.url);
-
+  //$newItem.addTagsAsClasses();
 
 // this.relDate = parseInt((new Date() - new Date(this.date))/60/60/24/1000);
  this.relDate = parseInt((new Date() - new Date(this.date))/60/60/24/1000);
@@ -38,14 +41,19 @@ $newItem.find('img').attr('src', this.imgSrc);
 $newItem.find('img').css('width', '48%');
 $newItem.find('img').css('display', 'inline');
 
+$text = $newItem.find('.text');
 
-$newItem.find('span').html(this.about);
-$newItem.find('span').css('width', '50%');
-$newItem.find('span').css('float', 'right');
-$newItem.find('span').css('display', 'inline');
+
+$text.html(this.about);
+
+this.displayFirstPara($text);
+
+$text.css('width', '50%');
+$text.css('float', 'right');
+$text.css('display', 'inline');
 
 $newItem.find('.dat').text('published '+this.relDate+' days ago');
-$newItem.find('.dat').css('float', 'left');
+
 
 
 $newItem.removeClass('template');
@@ -53,6 +61,26 @@ $newItem.removeClass('template');
 return $newItem;
  //return "<a href='"+this.url+"' target='_blank'>"+'<div class="item"> <img src='+this.imgSrc+'> <p><strong>'+this.name+'</strong><br>  '+this.relDate+' days ago <br><br>'+this.about+"</p></div></a>";
 }
+
+
+
+Item.prototype.displayFirstPara = function($text) {
+  $fPar = $text.find('p').eq(0);
+  $text.html($fPar);
+
+  $button = $('<button/>');
+  $button.text('more plz');
+  var item = this;
+  $button.on('click', function(e) {
+   item.displayFull($text);
+  })
+
+ $text.append($button);
+}
+
+Item.prototype.displayFull = function($text) {
+  $text.html(this.about);
+};
 
 rawData.sort(function(a,b) {
   return (new Date(b.date)) - (new Date(a.date));
--- scripts/rawItems.js
+++ scripts/rawItems.js
@@ -6,7 +6,7 @@ var rawData = [
     imgSrc: 'http://ankihg.ucoz.com/pdx_br/portland_bridges.png',
     date:'2015-12-10',
     tags: ['HTML', 'CSS', 'Javascript', 'Google Maps API'],
-    about: 'Like an interstate, the Willamette River tears the metropolis. Cross me to connect. Explore bridges and other crossings in the U.S. state of Oregon.'
+    about: '<p>Like an interstate, the Willamette River tears the metropolis. Cross me to connect. Explore bridges and other crossings in the U.S. state of Oregon.</p><p>Check it out.</p>'
   },
   {
     name: 'ZANREADS . INFO',
@@ -14,7 +14,7 @@ var rawData = [
     imgSrc: 'http://ankihg.ucoz.com/zanreads/zanreads2.png',
     date:'2015-10-31',
     tags: ['HTML', 'CSS', 'Javascript'],
-    about: 'My friend and client Zan requested a spooky website to display book reviews. <br> <br>&nbsp; &nbsp; &nbsp; &nbsp; Midnight moon illuminates literary alarm <br>&nbsp; &nbsp; &nbsp; &nbsp; Fiction is found in the forest <br> &nbsp; &nbsp; &nbsp; &nbsp; Ghosts grab ominous novels <br>&nbsp; &nbsp; &nbsp; &nbsp; Eerie reads for a cyberchill to the bone'
+    about: '<p>My friend and client Zan requested a spooky website to display book reviews.</p> <br>&nbsp; &nbsp; &nbsp; &nbsp; Midnight moon illuminates literary alarm <br>&nbsp; &nbsp; &nbsp; &nbsp; Fiction is found in the forest <br> &nbsp; &nbsp; &nbsp; &nbsp; Ghosts grab ominous novels <br>&nbsp; &nbsp; &nbsp; &nbsp; Eerie reads for a cyberchill to the bone'
   },
   {
     name: 'SEATTLE SKYLINE',
@@ -22,7 +22,7 @@ var rawData = [
     imgSrc: 'http://ankihg.ucoz.com/seattle_skyline/screenshots/seattle_skyline1.png',
     date:'2015-10-05',
     tags: ['HTML', 'CSS', 'Javascript', 'Google Maps API'],
-    about: 'An interactive map to explore the skyline of Seattle, this served as my platform to gain proficiency with Google Maps Javascript API. It is written in JavaScript and uses several Google Maps features including custom markers, events, and animations. Familiarize yourself or assess your intimacy with the edifices.'
+    about: '<p>An interactive map to explore the skyline of Seattle, this served as my platform to gain proficiency with Google Maps Javascript API. It is written in JavaScript and uses several Google Maps features including custom markers, events, and animations. Familiarize yourself or assess your intimacy with the edifices.</p>'
   },
   {
     name: 'WILLAMETTECOLLEGIAN . COM',
@@ -30,6 +30,6 @@ var rawData = [
     imgSrc: 'http://ankihg.ucoz.com/wu_col/img1.png',
     date:'2015-3-15',
     tags: ['HTML', 'CSS', 'WordPress'],
-    about: "I served as webmaster for my university's newspaper. This was my first expoersure to web development. Many of my redesigns remain active."
+    about: "<p>I served as webmaster for my university's newspaper. This was my first expoersure to web development. Many of my redesigns remain active.</p>"
   },
 ];
