a84dd1fbf7acda7d0f76019342da0588e91b9319
ankihg <hagelin.annika@gmail.com>
2016-01-04 19:56:03 -0800

    github link added. style change, more raw data.

--- scripts/rawItems.js
+++ scripts/rawItems.js
@@ -10,5 +10,14 @@ var rawData = [
     imgSrc: 'http://ankihg.ucoz.com/zanreads/zanreads2.png',
     about: 'My friend and client Zan requested a spooky website to display book reviews. <br> <br>&nbsp; &nbsp; &nbsp; &nbsp; Midnight moon illuminates literary alarm <br>&nbsp; &nbsp; &nbsp; &nbsp; Fiction is found in the forest <br> &nbsp; &nbsp; &nbsp; &nbsp; Ghosts grab ominous novels <br>&nbsp; &nbsp; &nbsp; &nbsp; Eerie reads for a cyberchill to the bone'
   },
-
+  {
+    name: 'SEATTLE SKYLINE',
+    imgSrc: 'http://ankihg.ucoz.com/seattle_skyline/screenshots/seattle_skyline1.png',
+    about: 'An interactive map to explore the skyline of Seattle, this served as my platform to gain proficiency with <a href="https://developers.google.com/maps/documentation/javascript/" target="_blank">Google Maps Javascript API</a>. It is written in JavaScript and uses several Google Maps features including custom markers, events, and animations. Familiarize yourself or assess your intimacy with the edifices.'
+  },
+  {
+    name: 'WILLAMETTECOLLEGIAN . COM',
+    imgSrc: 'http://ankihg.ucoz.com/wu_col/img1.png',
+    about: "I served as webmaster for my university's newspaper. This was my first expoersure the web development. Many of my redesigns remain active."
+  },
 ];
